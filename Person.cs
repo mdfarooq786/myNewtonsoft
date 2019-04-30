2302a8d9953db919ff6168464a75c0e3b03d22f1
mdfarooq786 <mdfarooq786@gmail.com>
2019-04-30 16:38:51 +0300

    myNewtonsoft

---
+++ Person.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Person
+{
+    public string Name { get; set; }    
+    public string Email { get; set; }
+    public DateTime DOB { get; set; }   
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace myNewtonSoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Person person = new Person{
+                Name = "Farooq",
+                Email = "f@f.com",
+                DOB = new DateTime(1983, 5, 10, 0, 0, 0, DateTimeKind.Utc),
+            };
+
+            Console.WriteLine("Person Info!");
+            Console.WriteLine("Name = {0}",person.Name);
+            Console.WriteLine("Email = {0}",person.Email);
+            Console.WriteLine("Name = {0}\n",person.DOB);
+
+            string personJSON = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine(personJSON);
+        }
+    }
+}
